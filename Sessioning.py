923b4c7b0482e36fb7bb94c4ec7bd5fd68f5c28f
Dan Zaratsian <dtz001@gmail.com>
2016-11-30 14:04:38 -0500

    Update Sessioning.py

--- Sessioning.py
+++ Sessioning.py
@@ -6,13 +6,13 @@
 #
 ###############################################################################################################
 
-import re
+import re,sys
 import datetime
 from pyspark.sql import Row
 from pyspark.sql.types import *
 from pyspark.sql.window import Window
 from pyspark.sql.functions import rank, lag
-from pyspark.sql.functions import udf
+from pyspark.sql.functions import udf, sum
 
 ##################################################################################
 #
@@ -63,11 +63,11 @@ recordsDF = records.map(cleanrecord).toDF(("host","datetime","type","request","s
 
 ##################################################################################
 #
+#   Capture previous (Lag) datetime of session.
+#   This will be used to calculate time since last visit / session duration.
+#
 #   Partition by HOST
 #   Sort by DATETIME
-#
-#   Capture previous datetime of session.
-#   This will be used to calculate time since last visit / session duration.
 #
 ##################################################################################
 
@@ -131,6 +131,50 @@ sessions2 = sessions1.withColumn('duration', f(sessions1.previous_time, sessions
 +--------------+---------------------+----+------------------------------------------------------+-----------+------+---------------------+--------+
 '''
 
+##################################################################################
+#
+#   Calculate number of sessions for each host
+#   A session will "timeout" after 900 seconds (15 minutes) of inactivity.
+#
+##################################################################################
+
+def create_timeout_flag(duration):
+    session_timeout = 900  # 15 minutes
+    out = 0
+    if (duration >= session_timeout) or (duration==0):
+        out = 1
+    
+    return out
+
+# Register as a UDF
+f = udf(create_timeout_flag, IntegerType())
+
+sessions2.withColumn('timeout_flag', f(sessions2.duration)).registerTempTable("sessions3")
+
+sessions4 = sqlContext.sql("""
+    SELECT *,
+    sum(timeout_flag) OVER (PARTITION BY host ORDER BY datetime) as number_of_sessions    
+    FROM sessions3
+    """)
+
+'''
++--------------+---------------------+----+------------------------------------------------------+-----------+------+---------------------+--------+------------+------------------+
+|host          |datetime             |type|request                                               |status_code|bytes |previous_time        |duration|timeout_flag|number_of_sessions|
++--------------+---------------------+----+------------------------------------------------------+-----------+------+---------------------+--------+------------+------------------+
+|128.158.26.109|1995-07-05 13:04:31.0|GET |/shuttle/missions/sts-71/images/KSC-95EC-0917.jpg     |200        |52491 |null                 |0       |0           |0                 |
+|128.158.26.109|1995-07-05 13:36:34.0|GET |/shuttle/missions/sts-71/images/KSC-95EC-0912.jpg     |200        |66202 |1995-07-05 13:04:31.0|1923    |1           |1                 |
+|128.158.26.109|1995-07-05 13:51:24.0|GET |/shuttle/technology/sts-newsref/stsref-toc.html       |200        |84907 |1995-07-05 13:36:34.0|890     |0           |1                 |
+|128.158.26.109|1995-07-05 13:52:45.0|GET |/shuttle/technology/sts-newsref/sts_asm.html          |200        |71656 |1995-07-05 13:51:24.0|81      |0           |1                 |
+|128.158.26.109|1995-07-05 13:53:02.0|GET |/shuttle/technology/images/srb_mod_compare_3-small.gif|200        |55666 |1995-07-05 13:52:45.0|17      |0           |1                 |
+|128.158.26.109|1995-07-05 14:37:37.0|GET |/shuttle/technology/images/srb_mod_compare_3.jpg      |200        |258334|1995-07-05 13:53:02.0|2675    |1           |2                 |
+|128.158.44.230|1995-07-06 11:54:06.0|GET |/shuttle/technology/sts-newsref/spacelab.html         |200        |104916|null                 |0       |0           |0                 |
+|128.158.44.230|1995-07-10 14:56:37.0|GET |/shuttle/technology/sts-newsref/spacelab.html         |200        |104916|1995-07-06 11:54:06.0|356551  |1           |1                 |
+|128.158.44.230|1995-07-17 12:06:20.0|GET |/shuttle/technology/sts-newsref/spacelab.html         |200        |104914|1995-07-10 14:56:37.0|594583  |1           |2                 |
+|128.158.44.230|1995-07-20 14:55:02.0|GET |/shuttle/technology/sts-newsref/stsref-toc.html       |200        |84905 |1995-07-17 12:06:20.0|269322  |1           |3                 |
++--------------+---------------------+----+------------------------------------------------------+-----------+------+---------------------+--------+------------+------------------+
+'''
+
+sessions4.groupBy("host").avg("duration").alias("avg_duration").show(10,False)
 
 
 
